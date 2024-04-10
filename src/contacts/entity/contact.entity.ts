778fa54a16e261ac41e278fee4fdc968bf3f4f34
Brabec <lu_brabec@hotmail.com>
2024-04-10 20:58:28 -0300

    Criação de Entity
    Definição de Entidade representante dos Contatos a serem gerenciados

--- src/contacts/entity/contact.entity.ts
+++ src/contacts/entity/contact.entity.ts
@@ -1,9 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class ContactEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   email: string;
+
+  @Column({ length: 1 })
   sex: string;
+
+  @Column()
   birth: Date;
+
+  @Column()
   maritalStatus: string;
   // address: AddressEntity;
 }
